9858be9abe4f7d0371b254ceace9be277979c4c3
Wiut student <cw1.fit.00007510@gmail.com>
2020-05-21 17:14:24 +0500

    Header is readey
    Navigation throgh the site is ready

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import HomePage from "./pages/homepage/homepage.component";
 
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route exact path="/" component={ HomePage }/>
         <Route path="/shop" component={ ShopPage }/>
