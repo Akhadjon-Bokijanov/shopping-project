7950711828276b0093097334ac225c4df6797091
Wiut student <cw1.fit.00007510@gmail.com>
2020-05-22 23:44:45 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,20 +6,42 @@ import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInSignUp from './pages/sign-in/sign-in.component';
+import {auth} from '../src/firebase/firebase.utils';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Switch>
-        <Route exact path="/" component={ HomePage }/>
-        <Route path="/shop" component={ ShopPage }/>
-        <Route path="/signin" component={ SignInSignUp }/>
-      </Switch>
-      {/* <HomePage/> */}
-    </div>
-  );
+class App extends React.Component {
+
+  constructor()
+  {
+    super()
+
+    this.state = {
+      currentUser: ''
+    }
+  }
+
+  componentDidMount()
+  {
+    auth.onAuthStateChanged(user => {
+      this.setState({
+        currentUser: user
+      })
+      console.log(user);
+    })
+  }
+
+  render()
+  {
+    return (
+      <div className="App">
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={ HomePage }/>
+          <Route path="/shop" component={ ShopPage }/>
+          <Route path="/signin" component={ SignInSignUp }/>
+        </Switch>
+      </div>
+    );
 }
 
 export default App;
