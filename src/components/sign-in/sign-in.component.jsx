1341cca6c25444cc9966bea74b3a856598c99532
Wiut student <cw1.fit.00007510@gmail.com>
2020-05-24 05:50:37 +0500

    Some design modifications
    Google sign in button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -67,10 +67,12 @@ class SignIn extends React.Component
                         onChange={this.handleChange}
                         required
                     />
-                    <CustomButton type="submit">Sign in</CustomButton>
-                <CustomButton isGoogleSignIn onClick={ signInWithGoogle }>
-                    Sign in with Google
-                </CustomButton>
+                    <div className="buttons">
+                        <CustomButton type="submit">Sign in</CustomButton>
+                        <CustomButton isGoogleSignIn onClick={ signInWithGoogle }>
+                            Sign in with Google
+                        </CustomButton>
+                    </div>
                 </form>
                 
             </div>
