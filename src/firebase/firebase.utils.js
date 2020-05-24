41649eb9c9bd63eb5c1b17a49c45f8349d6eb1e2
Wiut student <cw1.fit.00007510@gmail.com>
2020-05-24 06:27:27 +0500

    Storing in Firebase is enabled

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,6 +15,41 @@ const config = {
 
 firebase.initializeApp(config);
 
+//#region create user profile
+
+export const createUserProfileDocument = async(userAuth, additionalData) => {
+    if(!userAuth) return;
+    
+    
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    
+    const snapShot = await userRef.get()
+    // console.log(snapShot);
+
+    if(!snapShot.exists)
+    {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }
+        catch(error)
+        {
+            console.log("Error: not created new User: ", error);
+        }
+    }
+
+    return userRef;
+}
+
+//#endregion
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
