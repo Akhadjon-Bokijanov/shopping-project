6c6f62c410c7395cd5e2387ef17c0643bd091e84
Wiut student <cw1.fit.00007510@gmail.com>
2020-05-26 13:57:58 +0500

    Google fail

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -69,7 +69,7 @@ class SignIn extends React.Component
                     />
                     <div className="buttons">
                         <CustomButton type="submit">Sign in</CustomButton>
-                        <CustomButton onClick={ signInWithGoogle }>
+                        <CustomButton signInWithGoogle onClick={ signInWithGoogle }>
                             Sign in with Google
                         </CustomButton>
                     </div>
