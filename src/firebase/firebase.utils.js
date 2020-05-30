6fc14246c31d520f747c29f7ee4fdfe9c36d2d80
Wiut student <cw1.fit.00007510@gmail.com>
2020-05-30 13:33:53 +0500

    adding files to github pages

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,15 +2,8 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-    // apiKey: "AIzaSyD46RyCA3hmOfcJrKls-1G8op8zTtJrJ3w",
-    // authDomain: "my-shopping-project-8cb79.firebaseapp.com",
-    // databaseURL: "https://my-shopping-project-8cb79.firebaseio.com",
-    // projectId: "my-shopping-project-8cb79",
-    // storageBucket: "my-shopping-project-8cb79.appspot.com",
-    // messagingSenderId: "264303672184",
-    // appId: "1:264303672184:web:dce15ac84cc97b21ec268d",
-    // measurementId: "G-SVY651WV71"
+
+const firebaseConfig = {
     apiKey: "AIzaSyD46RyCA3hmOfcJrKls-1G8op8zTtJrJ3w",
     authDomain: "my-shopping-project-8cb79.firebaseapp.com",
     databaseURL: "https://my-shopping-project-8cb79.firebaseio.com",
@@ -19,14 +12,14 @@ const config = {
     messagingSenderId: "264303672184",
     appId: "1:264303672184:web:dce15ac84cc97b21ec268d",
     measurementId: "G-SVY651WV71"
-}
+  };
 
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 export const createUserProfileDocument = async(userAuth, additionalData) =>{
     if(!userAuth) return;
 
-    console.log(firestore.doc('users/13341eASF32'));
+    //console.log(firestore.doc('users/13341eASF32'));
 }
 
 
