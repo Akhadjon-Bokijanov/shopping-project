e96ded560d0e67f073c16d92fcfee90915151f29
Wiut student <cw1.fit.00007510@gmail.com>
2020-05-30 13:32:08 +0500

    Updates

---
+++ precache-manifest.6b509e381260ca1a5363b814352e71cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba33a82ceefcdeec9029868db4a8c50c",
+    "url": "/shopping-project/index.html"
+  },
+  {
+    "revision": "81a81d3c27680e798471",
+    "url": "/shopping-project/static/css/main.dd4db9f7.chunk.css"
+  },
+  {
+    "revision": "0c5d6d927732fd1adbf8",
+    "url": "/shopping-project/static/js/2.37897639.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/shopping-project/static/js/2.37897639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a81d3c27680e798471",
+    "url": "/shopping-project/static/js/main.975113b1.chunk.js"
+  },
+  {
+    "revision": "dcde8b7370565ee28480",
+    "url": "/shopping-project/static/js/runtime-main.9c961dee.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/shopping-project/static/media/logo.ed28accb.svg"
+  }
+]);
