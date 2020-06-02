9306ddc9d0230748c1b083f1c85b936dd68043eb
Wiut student <cw1.fit.00007510@gmail.com>
2020-06-02 18:39:01 +0500

    Redux persist is implemented
    yarn add redux-persist

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {Provider} from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import store from './redux/store';
+import {store, persistor} from './redux/store';
 import App from './App';
+import { PersistGate } from "redux-persist/integration/react";
+
+
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </React.StrictMode>
   </Provider>,
